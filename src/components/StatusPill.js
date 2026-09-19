da4be2d5fa1273d02dcc9311b5e6e051dd69bceb
MuhamedUllas <u2408056@rajagiri.edu.in>
2026-09-19 15:32:40 +0530

    Fresh start

--- src/components/StatusPill.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { statusTone } from "../utils/format";
-import "./StatusPill.css";
-
-export default function StatusPill({ status }) {
-  const tone = statusTone(status);
-  return <span className={`status-pill status-pill--${tone}`}>{status}</span>;
-}
